95ba6c0a2313b254373a18f48995e7b43626fbcf
ujjal zaman <muzzwal97@gmail.com>
2021-11-17 22:13:20 +0600

    update index file reviews and services

--- index.js
+++ index.js
@@ -1,88 +1,91 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('Mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5050;
 
-//middleware
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-require('dotenv').config();
+require('dotenv').config(); 
 
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cvfa5.mongodb.net/easyConstulting?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cvfa5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const serviceCollection = client.db(`${process.env.DB_NAME}`).collection("services");
+  const reviewCollection = client.db(`${process.env.DB_NAME}`).collection("review");
+  const orderCollection = client.db(`${process.env.DB_NAME}`).collection("orders");
+  const adminCollection = client.db(`${process.env.DB_NAME}`).collection("admin");
 
-client.connect((err) =>{
-	const serviceCollection = client.db(`${process.env.DB_NAME}`).collection("services");
-  	const reviewCollection = client.db(`${process.env.DB_NAME}`).collection("review");
-  	const orderCollection = client.db(`${process.env.DB_NAME}`).collection("orders");
-  	const adminCollection = client.db(`${process.env.DB_NAME}`).collection("admin");
+  const handlePost = (route, collection) => {
+    app.post(route, (req, res) => {
+      const data = req.body;
+      collection.insertOne(data)
+      .then(result => {
+          res.send(result.insertedCount > 0)
+      })
+    })
+  }
+  handlePost('/addService', serviceCollection)
+  handlePost('/addReview', reviewCollection)
+  handlePost('/addOrder', orderCollection)
+  handlePost('/addAdmin', adminCollection)
 
-  	const handlePost = (route, collection) =>{
-  		app.post(route, (req, res) =>{
-  			collection.insertOne(data)
-  			.then(result =>{
-  				res.send(result.insertedCount > 0)
-  			})
-  		})
-  	}
-  	handlePost('/addService', serviceCollection)
-  	handlePost('/addReview', reviewCollection)
-  	handlePost('/addOrder', orderCollection)
-  	handlePost('/addAdmin', adminCollection)
 
-  	const handleGet = (route, collection, findobj={}) =>{
-  		app.get(route, (req, res) =>{
-  			collection.find(
-  				findobj === 'email'? {email:req.query.email}:
-  				findobj === 'id' ? {__id : ObjectId(req.params.id)}: null
-  			)
-  			.toArray((err, items) =>{
-  				res.send(items)
-  			})
-  		})
+  const handleGet = (route, collection, findObj = {}) => {
+    app.get(route, (req, res) => {
+      collection.find(
+        findObj === 'email' ? {email: req.query.email}:
+        findObj === 'id'? {_id: ObjectId(req.params.id)}: null
+      )
+      .toArray((err, items) => {
+        res.send(items)
+      })
+    })
+  }
+  handleGet('/services', serviceCollection);
+  handleGet('/reviews', reviewCollection);
+  handleGet('/orders', orderCollection);
+  handleGet('/bookingList', orderCollection, 'email');
+  handleGet('/admin', adminCollection, 'email')
+  handleGet('/userReview', reviewCollection, 'email');
+  handleGet('/userReview/:id', reviewCollection, 'id');
 
-  	}
-  	handleGet('/services', serviceCollection);
-  	handleGet('/reviews', reviewCollection);
-  	handleGet('/orders', orderCollection);
-  	handleGet('/bookingList', orderCollection, 'email');
-  	handleGet('/admin', adminCollection, 'email');
-  	handleGet('/userReview', reviewCollection, 'email');
-  	handleGet('/userReview/:id', reviewCollection, 'id');
+  
+  const handleUpdate = (route, collection) => {
+    app.patch(route, (req, res) => {
+      collection.updateOne({_id: ObjectId(req.params.id)},{
+          $set: req.body
+      })
+      .then(result => {
+        res.send( result.modifiedCount > 0)
+      })
+    })
+  }
+  handleUpdate('/statusUpdate/:id', orderCollection);
+  handleUpdate('/updateReview/:id', reviewCollection);
+  handleUpdate('/updateService/:id', serviceCollection);
 
-  	const hanldeUpdate =(route, collection) =>{
-  		app.path(route, (req, res) =>{
-  			collection.updateOne({__id:ObjectId(req.params.id)}, {
-  				$set:req.body
-  			})
-  			.then(result=>{
-  				res.send((result.modifiedCount > 0))
-  			})
-  		})
-  	}
-  	hanldeUpdate('/statusUpdata/:id', orderCollection);
-  	hanldeUpdate('/updateReview/:id', reviewCollection);
-  	hanldeUpdate('/updateService/:id', serviceCollection);
 
-  	const handleDelete = (route, collection) =>{
-  		app.delete(route, (res, req) =>{
-  			collection.deleteOne({_id:ObjectId(req.params.id)})
-  			.then(result=>{
-  				res.send(result.deleteCount > 0)
-  			})
-  		})
-  	}
-  	handleDelete('/delete/:id', serviceCollection);
-  	handleDelete('/deleteReview/:id', reviewCollection);
-  	handleDelete('/deleteOrder/:id', orderCollection);
+  const handleDelete = (route, collection) => {
+    app.delete(route, (req, res) => {
+      collection.deleteOne({_id: ObjectId(req.params.id)})
+      .then(result => {
+        res.send( result.deletedCount > 0)
+      })
+    })
+  }
+  handleDelete('/delete/:id', serviceCollection);
+  handleDelete('/deleteReview/:id', reviewCollection);
+  handleDelete('/deleteOrder/:id', orderCollection);
+    
+});
 
-  	app.get('/', (req, res) =>{
-			res.send('welcome to page')
-			console.log("hellow worlde")
-  	})
-
+app.get('/', (req, res) => {
+	console.log("hellow workng")
+	res.send('welcome to easy consulting!')
+	
 })
 
-app.listen(process.env.PORT || port)
+app.listen(port)
